0349404ea7be95cf6faa82fa26eff6da00c06d62
Orthmox <nobleadagbe@gmail.com>
2024-01-26 23:47:58 +0000

    fixed findJane

--- scripts/findJane.sh
+++ scripts/findJane.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+# Get the absolute path of the directory containing the script
+script_dir=$(dirname "$(readlink -f "$0")")
 > oldFiles.txt
 
 files=$(grep "jane " ../data/list.txt | cut -d' ' -f3)
 
-for i in $files; do while test -e ../
+for i in $files; do 
+  if test -e ..$i; then
+  echo $script_dir$i >> oldFiles.txt;
+  fi
+done
